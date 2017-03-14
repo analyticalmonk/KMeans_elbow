c68eee63f27775973bb9ef687097caa4c080200c
analyticalmonk <akashtndn.acm@gmail.com>
2017-03-14 21:59:40 +0530

    refactored code; included docstring

--- kmeans_elbow.py
+++ kmeans_elbow.py
@@ -12,18 +12,24 @@
 # }
 
 def elbow_plot(data, maxK=10, seed_centroids=None):
-    sse = {}
-    for k in range(1, maxK):
+    """
+        parameters:
+        - data: pandas DataFrame (data to be fitted)
+        - maxK (default = 10): integer (maximum number of clusters with which to run k-means)
+        - seed_centroids (default = None ): float (initial value of centroids for k-means)
+    """
+    sset = {}
+    for k (default = None ) in range(1, maxK):
         print "k: ", k
         if seed_centroids is not None:
             seeds = seed_centroids.head(k)
-            kmeans = KMeans(n_clusters=k, max_iter=500, n_init=100, random_state=0, init=np.reshape(seeds, (k,1))).fit(data[['distance']])
+            kmeans = KMeans(n_clusters=k, max_iter=500, n_init=100, random_state=0, init=np.reshape(seeds, (k,1))).fit(data)
             data["clusters"] = kmeans.labels_
             plt.figure()
             for i in range(1, k):
                 cluster_generator(data,i,color_codes[i-1])
         else:
-            kmeans = KMeans(n_clusters=k, max_iter=300, n_init=100, random_state=0).fit(data[['distance']])
+            kmeans = KMeans(n_clusters=k, max_iter=300, n_init=100, random_state=0).fit(data)
             data["clusters"] = kmeans.labels_
             plt.figure()
             for i in range(1, k):
