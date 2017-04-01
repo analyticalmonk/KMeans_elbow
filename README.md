c629ab38c865a26caf003432b5f6432c26594ba5
analyticalmonk <akashtndn.acm@gmail.com>
2017-04-01 19:58:11 +0530

    Updated ipython notebook, readme and code file for elbow plot generation

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # KMeans_elbow
-Python code for:
-- Fitting [scikit-learn](http://scikit-learn.org/)'s [K-Means](http://scikit-learn.org/stable/modules/generated/sklearn.cluster.KMeans.html) model to the [iris dataset](https://archive.ics.uci.edu/ml/datasets/Iris).
-- Determining optimal number of clusters for K-means algorithm using the 'elbow criterion'.
+Repository contains:
+- Code for fitting [scikit-learn](http://scikit-learn.org/)'s [K-Means](http://scikit-learn.org/stable/modules/generated/sklearn.cluster.KMeans.html) model to the [iris dataset](https://archive.ics.uci.edu/ml/datasets/Iris).
+- Code for determining optimal number of clusters for K-means algorithm using the 'elbow criterion'.
+- IPython notebook combining the above two as an interactive tutorial.
 
 Reference:
 - [Using the elbow method to determine the optimal number of clusters for k-means clustering](https://bl.ocks.org/rpgove/0060ff3b656618e9136b)
--- kmeans_elbow.py
+++ kmeans_elbow.py
@@ -1,15 +1,6 @@
-# Elbow function
-# var sse = {};
-# for (var k = 1; k <= maxK; ++k) {
-#     sse[k] = 0;
-#     clusters = kmeans(dataset, k);
-#     clusters.forEach(function(cluster) {
-#         mean = clusterMean(cluster);
-#         cluster.forEach(function(datapoint) {
-#             sse[k] += Math.pow(datapoint - mean, 2);
-#         });
-#     });
-# }
+import pandas as pd
+from sklearn.cluster import KMeans
+
 
 def elbow_plot(data, maxK=10, seed_centroids=None):
     """
@@ -20,24 +11,19 @@ def elbow_plot(data, maxK=10, seed_centroids=None):
     """
     sse = {}
     for k in range(1, maxK):
-        print "k: ", k
+        print("k: ", k)
         if seed_centroids is not None:
             seeds = seed_centroids.head(k)
             kmeans = KMeans(n_clusters=k, max_iter=500, n_init=100, random_state=0, init=np.reshape(seeds, (k,1))).fit(data)
             data["clusters"] = kmeans.labels_
-#            plt.figure()
-#            for i in range(1, k):
-#                cluster_generator(data,i,color_codes[i-1])
         else:
             kmeans = KMeans(n_clusters=k, max_iter=300, n_init=100, random_state=0).fit(data)
             data["clusters"] = kmeans.labels_
-#            plt.figure()
-#            for i in range(1, k):
-#                cluster_generator(data,i,color_codes[i-1])
         # Inertia: Sum of distances of samples to their closest cluster center
         sse[k] = kmeans.inertia_
     plt.figure()
-    plt.plot(sse.keys(), sse.values())
+    plt.plot(list(sse.keys()), list(sse.values()))
+    plt.show()
     return
 
 # elbow_plot(data)
